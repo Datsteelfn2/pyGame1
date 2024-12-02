a902308f39c59e5f10c863a0e7793f833bc701a5
Elhadj D <Datsteelfn@gamil.com>
2024-12-02 18:29:44 -0500

    Got the background and block set

---
+++ main.py
@@ -0,0 +1,48 @@
+import pygame
+import time
+import random
+
+
+WIDTH, HEIGHT=1000,800
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Space Dodge")
+
+
+BG=pygame.transform.scale(pygame.image.load("background.png"),(WIDTH, HEIGHT))
+
+
+PLAYER_WIDTH=40
+PLAYER_HEIGHT=60
+
+
+def draw(player):
+    WIN.blit(BG,(0,0))
+   
+    pygame.draw.rect(WIN,"red",player)
+
+
+    pygame.display.update()
+
+
+#hey
+
+
+def main():
+    run=True
+
+
+    player=pygame.Rect(200,HEIGHT-PLAYER_HEIGHT,PLAYER_WIDTH,PLAYER_HEIGHT)
+
+
+    while run:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                run=False
+                break
+        draw(player)
+    pygame.quit()
+
+
+if __name__=="__main__":
+    main()
+   
