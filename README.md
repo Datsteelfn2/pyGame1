8ad56766b9db18cd778e543554018b8795ed4455
Elhadj D <Datsteelfn@gamil.com>
2024-12-02 19:28:02 -0500

    Got the red object to move left and right without exceeding the window

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pyGame1
+This python game basically serves trhe purpose of a red block dodging a bunch of raindrops, if the block hits a raindrop the game ends
--- main.py
+++ main.py
@@ -15,6 +15,8 @@ PLAYER_WIDTH=40
 PLAYER_HEIGHT=60
 
 
+PLAYER_VEL= 5
+
 def draw(player):
     WIN.blit(BG,(0,0))
    
@@ -33,12 +35,22 @@ def main():
 
     player=pygame.Rect(200,HEIGHT-PLAYER_HEIGHT,PLAYER_WIDTH,PLAYER_HEIGHT)
 
-
+    clock=pygame.time.Clock()
     while run:
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 run=False
                 break
+
+
+        keys=pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x-PLAYER_VEL>=0:
+            player.x-=PLAYER_VEL
+
+        if keys[pygame.K_RIGHT] and player.x+PLAYER_VEL+player.width<=WIDTH:
+            player.x+=PLAYER_VEL
+        
         draw(player)
     pygame.quit()
 
